2189a2133d8c731aceb3e22079c7eb2643b6e314
leo900807 <leo900807@yahoo.com.tw>
2023-08-29 21:50:41 +0800

    Fix lint error

--- src/createDB.ts
+++ src/createDB.ts
@@ -3,11 +3,11 @@ import { AppDataSource } from "./data-source";
 
 (async () => {
     createDatabase({ ifNotExist: true, synchronize: false }).then(async() => {
-        AppDataSource.initialize().then(async connection => {
+        await AppDataSource.initialize().then(async connection => {
             await connection.synchronize();
             await connection.runMigrations({ fake: true });
             console.log("Database was successfully created.");
-            connection.close();
+            await connection.close();
         });
     }).catch(err => console.log(err));
-})();
+})().catch(err => console.log(err));
